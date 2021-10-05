35f6f45c00c677ed0019ab6cc7a333a90180ea4c
Dahir Abdi <dahirabdi@Dahirs-MacBook-Pro.local>
2021-10-04 21:28:14 -0400

    teamgenerator a little issues

---
+++ develop/index.js
@@ -0,0 +1,140 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+const { restoreDefaultPrompts } = require("inquirer");
+
+const roleInput = [
+    {
+        type: "list",
+        name: "role",
+        message: "Please select your role:",
+        choices: 
+        [ "Manager", "Engineer", "Intern", "Continue" ]
+    },
+];    
+
+const staffInput = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter your name:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address:",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter your ID:",
+    }
+];  
+
+const engineerInput = [
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your Github username:",
+    } 
+];
+
+const internInput = [
+    {
+        type: "input",
+        name: "school",
+        message: "Please enter your school name:",
+    } 
+];
+
+const managerInput = [
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter your office number:",
+    } 
+];
+
+const employees = [];
+
+function teamGenerator() {
+    inquirer
+        .prompt([...roleInput])
+        .then((data) => {
+        if (data.role === "Engineer") {
+            createEngineer();
+        } else if(data.role === "Intern") {
+            createIntern();
+        } else if(data.role === "Manager") {
+            createManager();
+        } else { 
+            console.log(employees)
+            console.log("Thank you for using the Team Profile Generator.")
+            writeToFile("./output/team.html", render(employees))
+        }
+    })
+
+    createEngineer = () => {
+        return inquirer
+            .prompt([...staffInput, ...engineerInput])
+            .then(({ name, id, email, github }) => {
+            let engineer = new Engineer(name, id, email, github);
+        if (name === "" && id === "" && email === "" && github === "") {
+            console.log("Incorrect entry. Try again.");
+            createEngineer();
+        } else {
+            employees.push(engineer);
+            teamGenerator();
+        };
+    });
+    }
+
+    createIntern = () => {
+        return inquirer
+            .prompt([...staffInput, ...internInput])
+            .then(({ name, id, email, school }) => {
+            let intern = new Intern(name, id, email, school);
+        if (name === "" && id === "" && email === "" && school === "") {
+            console.log("Incorrect entry. Try again.");
+            createIntern();
+        } else {
+            employees.push(intern);
+            teamGenerator();
+            };
+    });
+    }
+
+    createManager = () => {
+        return inquirer
+            .prompt([...staffInput, ...managerInput])
+            .then(({ name, id, email, officeNumber }) => {
+            let manager = new Manager(name, id, email, officeNumber);
+        if (name === "" && id === "" && email === "" && officeNumber === "" && (officeNumber)) {
+            console.log("Incorrect entry. Try again.");
+            createManager();
+        } else {
+            employees.push(manager);
+            teamGenerator();
+            };
+    });
+    }
+    
+    writeToFile = (filename, data) => {
+        fs.writeFile(filename, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Your Team HTML has been created.");
+    })
+    }
+}
+
+teamGenerator();
